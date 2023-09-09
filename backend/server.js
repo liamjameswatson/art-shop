bf82fa508dc12a1db372c7e098ef20b338c5acb0
Liam <liamjwatson@aol.com>
2023-09-09 15:54:58 +0100

    .env file added, (not pushed to github)

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config()
 import { products } from "./data/placeholderData.js";
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 const app = express();
 
