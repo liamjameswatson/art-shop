ffd78b7a5d20b14ad958728fbd68af63f251fad0
Liam <liamjwatson@aol.com>
2023-09-16 15:56:52 +0100

    utils folder added, moved cart logic to there, for better readabilty

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,14 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { updateCart } from "../utils/cartUtils";
 
 // get from local storage or create an object with empty array
 const initialState = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart"))
   : { cartItems: [] };
-
-// helper function for decimal place
-const addDecimals = (num) => {
-  return (Math.round(num * 100) / 100).toFixed(2);
-};
 
 const cartSlice = createSlice({
   name: "cart",
@@ -27,31 +23,8 @@ const cartSlice = createSlice({
       } else {
         state.cartItems = [...state.cartItems, item];
       }
-      // Calculate item price
-      state.itemsPrice = addDecimals(
-        state.cartItems.reduce(
-          (acc, item) => acc + item.price * item.quantity,
-          0
-        )
-      );
 
-      // Calculate delivery price (If order over £100 shipping is free, else £10)
-      state.deliveryPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
-
-      // Calculate tax price (15% tax)
-      state.taxPrice = addDecimals(
-        Number((state.itemsPrice * 0.15).toFixed(2))
-      );
-
-      // Calculate total price
-      state.totalPrice = (
-        Number(state.itemsPrice) +
-        Number(state.taxPrice) +
-        Number(state.deliveryPrice)
-      ).toFixed(2);
-
-      // save state to local storage
-      localStorage.setItem("cart", JSON.stringify(state));
+      return updateCart(state);
     },
   },
 });
---
+++ frontend/src/utils/cartUtils.js
@@ -0,0 +1,29 @@
+// helper function for decimal place
+export const addDecimals = (num) => {
+  return (Math.round(num * 100) / 100).toFixed(2);
+};
+
+export const updateCart = (state) => {
+  // Calculate item price
+  state.itemsPrice = addDecimals(
+    state.cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0)
+  );
+
+  // Calculate delivery price (If order over £100 shipping is free, else £10)
+  state.deliveryPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
+
+  // Calculate tax price (15% tax)
+  state.taxPrice = addDecimals(Number((state.itemsPrice * 0.15).toFixed(2)));
+
+  // Calculate total price
+  state.totalPrice = (
+    Number(state.itemsPrice) +
+    Number(state.taxPrice) +
+    Number(state.deliveryPrice)
+  ).toFixed(2);
+
+  // save state to local storage
+  localStorage.setItem("cart", JSON.stringify(state));
+
+  return state;
+};
