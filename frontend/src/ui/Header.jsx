70d7a56c4a873cd09b0d69f7fefade4099f9e459
Liam <liamjwatson@aol.com>
2023-09-16 16:39:05 +0100

    number displaying next to cart in navbar

--- frontend/src/ui/Header.jsx
+++ frontend/src/ui/Header.jsx
@@ -1,8 +1,13 @@
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Navbar, Nav, Container, Badge } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
 import { LinkContainer } from "react-router-bootstrap";
 
+import { useSelector } from "react-redux";
+
 function Header() {
+  // get the cart items from the cart
+  const { cartItems } = useSelector((state) => state.cart);
+
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
@@ -17,6 +22,12 @@ function Header() {
                 <Nav.Link>
                   <FaShoppingCart />
                   Cart
+                  {cartItems.length > 0 && (
+                    <Badge pill bg="success" style={{ marginLeft: "7px" }}>
+                      {/* reducer func rather than .length to get quantity */}
+                      {cartItems.reduce((a, c) => a + c.quantity, 0)}
+                    </Badge>
+                  )}
                 </Nav.Link>
               </LinkContainer>
               <LinkContainer to="/login">
