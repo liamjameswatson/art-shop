455252ee1740eb653d7c293155212d422241a57f
Liam <liamjwatson@aol.com>
2023-09-17 21:52:38 +0100

    register user function added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,7 +2,7 @@ import asyncHandler from "../middleware/asyncHandler.js";
 import User from "../models/userModel.js";
 import jwt from "jsonwebtoken";
 
-// @desc login, Authenticate user & get token
+// @desc Login, Authenticate user & get token
 // @route POST /api/users/login
 // @access Public
 
@@ -47,7 +47,34 @@ const authUser = asyncHandler(async (req, res) => {
 // @access Public
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.send("reigister user");
+  const { name, email, password, role } = req.body;
+
+  // Check for existing user
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400); // 400 client error
+    throw new Error("User already exists");
+  }
+
+  // else create new user
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    // if user send this back
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 });
 
 // @desc Logout user && clear cookie
