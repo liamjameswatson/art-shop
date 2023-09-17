c72ee7b8ac9d7b4f8c00b365b0a967977c4a30a2
Liam <liamjwatson@aol.com>
2023-09-17 20:09:53 +0100

    logout

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -55,7 +55,12 @@ const registerUser = asyncHandler(async (req, res) => {
 // @access Private
 
 const logoutUser = asyncHandler(async (req, res) => {
-  res.send("logout user");
+  // set cookie to empty string
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+  res.status(200).json({ message: "Logged out successfully" });
 });
 
 // @desc Get user profile
