c3ed8ba60f889d1c49e9e6535c42719ab152a948
Liam <liamjwatson@aol.com>
2023-09-18 15:40:02 +0100

    header displays either login / or user name, with a link to profile and logout

--- frontend/src/ui/AppLayout.jsx
+++ frontend/src/ui/AppLayout.jsx
@@ -1,13 +1,21 @@
 import { Outlet } from "react-router-dom";
+import { Container } from "react-bootstrap";
 import Header from "./Header";
 import Footer from "./Footer";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function AppLayout() {
   return (
     <>
       <Header />
-      <Outlet />
+      <main className="py-3">
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
       <Footer />
+      <ToastContainer />
     </>
   );
 }
--- frontend/src/ui/Header.jsx
+++ frontend/src/ui/Header.jsx
@@ -1,4 +1,4 @@
-import { Navbar, Nav, Container, Badge } from "react-bootstrap";
+import { Navbar, Nav, Container, Badge, NavDropdown } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
 import { LinkContainer } from "react-router-bootstrap";
 
@@ -7,6 +7,11 @@ import { useSelector } from "react-redux";
 function Header() {
   // get the cart items from the cart
   const { cartItems } = useSelector((state) => state.cart);
+  const { userInfo } = useSelector((state) => state.auth);
+
+  const logoutHandler = () => {
+    console.log("logout");
+  };
 
   return (
     <header>
@@ -30,12 +35,24 @@ function Header() {
                   )}
                 </Nav.Link>
               </LinkContainer>
-              <LinkContainer to="/login">
-                <Nav.Link>
-                  <FaUser />
-                  Login
-                </Nav.Link>
-              </LinkContainer>
+              {userInfo ? (
+                <NavDropdown title={userInfo.name} id="username">
+                  <LinkContainer to="/profile">
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </LinkContainer>
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    {" "}
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <LinkContainer to="/login">
+                  <Nav.Link>
+                    <FaUser />
+                    Login
+                  </Nav.Link>
+                </LinkContainer>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
